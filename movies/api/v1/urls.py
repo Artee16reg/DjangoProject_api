0f805fd074dafed9a29af17bb15d63da386bd34b
kznar <kznartee16reg@gmail.com>
2021-07-30 12:03:50 +0300

    changed folder structure for rest

---
+++ movies/api/v1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'v1'
+
+urlpatterns = [
+    path("movie/", views.MovieListView.as_view()),
+    # path("movie/<int:pk>/", views.MovieDetailView.as_view())
+]
